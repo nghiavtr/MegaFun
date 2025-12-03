313ce44971c23e6561e22133f57c623dfb3dce85
Trung Nghia Vu <nghiavtr@users.noreply.github.com>
2025-12-03 16:56:53 +0100

    Fix the mismatched gene names in singletons

--- combine_Y.R
+++ combine_Y.R
@@ -47,6 +47,7 @@ load(paste0(pan_eqv_dir, "/Est_result_Singletons.RData"))
 suppressMessages(suppressWarnings(library(Biostrings)))
 pan_genome = readDNAStringSet(panfn)
 pan_name=names(pan_genome)
+
 pan_shortname = sapply(pan_name, function(s) {
   s = unlist(strsplit(s, "\\."))
   s = s[length(s)]
@@ -59,6 +60,21 @@ pan_long2short=pan_shortname
 names(pan_long2short)=pan_name
 pan_short2long=pan_name
 names(pan_short2long)=pan_shortname
+
+
+#nghiavtr-03/12/2025 - begin: fix the mismatched names
+pan_name2 = sapply(pan_name, function(s) {
+  s1 = unlist(strsplit(s, "\\."))
+  s1[1]=gsub("PAN","",s1[1])
+  paste0(s1[1], s1[3])
+})
+names(pan_name2)=NULL
+pan_long2short=pan_shortname
+names(pan_long2short)=pan_name2
+pan_short2long=pan_name2
+names(pan_short2long)=pan_shortname
+#nghiavtr-03/12/2025 - end
+
 
 convertPanName <-function(mat){
   xname=rownames(mat)
